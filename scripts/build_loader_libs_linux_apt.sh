cc2e9ccce2fa777dd29b515e31bbe9cac3539b1e
kioku <oku@kashika.co.jp>
2019-03-12 16:40:24 +0900

    fixed apt.sh

--- scripts/build_loader_libs_linux_apt.sh
+++ scripts/build_loader_libs_linux_apt.sh
@@ -236,7 +236,7 @@ function build_compositor {
 	#	make distclean
 	#fi
 
-	CXX=${cxx_compiler} CC=${c_compiler} CFLAGS="${c_flags} -fopenmp" CXXFLAGS="${cxx_flags} -fopenmp" ./configure --prefix=${installdir}/234Compositor_AVR && make && make install
+	CXX=${cxx_compiler} CC=${c_compiler} CFLAGS="${c_flags} -fopenmp" CXXFLAGS="${cxx_flags} -fopenmp" ./configure --prefix=${installdir}/234Compositor && make && make install
 	cd ${topdir}
 }
 
